124d47439c791ac2d4fe4f818c8fd19394474bda
Your Name <you@example.com>
2022-09-29 19:45:25 +0300

    both codes

--- lab_3.1/Source.cpp
+++ lab_3.1/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+/*
 int main ()
 {
 	double x;//number
@@ -19,6 +19,27 @@ int main ()
 			y2 = sqrt(1.0 + abs(pow(cos(x), 3)));
 	y = f - y2;
 	cout << y << endl;
+	*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	double x;//number
+	double y; // result
+	double y1; // between
+	double f;//1.0 / abs(x + 2) + 1
+	cout << "Enter a number: "; cin >> x;
+	f = 1.0 / abs(x + 2) + 1;
+	if (x < 1)
+		y1 = 7 * x * x + x - 8;
+	if (1 <= x && x <= 4)
+		y1 = 1.0 / (tan((x + 4.0) / sqrt(11))) + 3;
+	if (x > 4)
+		y1 = sqrt(1.0 + abs(pow(cos(x), 3)));
+	y = f - y1;
+	cout << y << endl;
+	return 0;
+}
 
 
 

6bf8754527766a03dab489e49dc94040bae2d89b
Your Name <you@example.com>
2022-10-03 19:32:49 +0300

    lab_3.4

---
+++ lab_3.4/Source.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main() {
+	double x;
+	double y;
+	double R;
+	cout << "Enter x: "; cin >> x;
+	cout << "Enter y: "; cin >> y;
+	cout << "Enter R: "; cin >> R;
+
+	if ((x <= 0 && x >= -R && y <= x + R && y >= 0 && y <= R) || (x >= 0 && x <= R && y <= x * x + y * y - R * R && y >= 0 && y <= R) || (x <= 0 && y <= x * x + y * y - R * R && y <= 0 && y >= -R && x>=-R))
+		cout << "yes" << endl;
+	else
+		cout << "no";
+
+
+
+
+
+
+
+
+
+	return 0;
+}
